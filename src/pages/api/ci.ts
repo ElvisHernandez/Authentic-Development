05fbb443c98a0e7e7d0ab913bce0e0b00119ccbb
Elvis Hernandez <elvishernandezdev@gmail.com>
2023-09-23 15:46:50 -0400

    update ci webhook

--- src/pages/api/ci.ts
+++ src/pages/api/ci.ts
@@ -7,69 +7,72 @@ const execPromisified = util.promisify(exec);
 export default api(async (req, res, ctx) => {
   const { secret } = req.body;
 
-  const response = {
+  const response: {
+    statusCode: number;
+    logs: string[];
+    errors: string[];
+  } = {
     statusCode: 200,
-    msg: "CI successfully completed",
-    error: "",
+    logs: [],
+    errors: [],
   };
 
   if (secret !== process.env.CI_SECRET) {
     response.statusCode = 401;
-    response.msg = "CI errored";
+    response.errors.push("Invalid Secret");
     return res.status(response.statusCode).json(response);
   }
 
   try {
+    response.logs.push(
+      "---------------------------CD into app directory---------------------------"
+    );
     const { stdout: stdout0, stderr: stderror0 } = await execPromisified(`cd /home/ubuntu/app`);
-    // console.log(`stdout: ${stdout1}`);
-    // console.log(`stderr: ${stderror1}`);
+    response.logs.push(stdout0);
+    response.errors.push(stderror0);
 
-    response.msg += stdout0 + "\n";
-    response.error += stderror0 + "\n";
-
+    response.logs.push(
+      "---------------------------Git pull latest changes---------------------------"
+    );
     const { stdout: stdout1, stderr: stderror1 } = await execPromisified(
       `GIT_SSH_COMMAND="ssh -i /home/ubuntu/deploy_key.pem" git pull`
     );
-    // console.log(`stdout: ${stdout1}`);
-    // console.log(`stderr: ${stderror1}`);
+    response.logs.push(stdout1);
+    response.errors.push(stderror1);
 
-    response.msg += stdout1 + "\n";
-    response.error += stderror1 + "\n";
-
+    response.logs.push(
+      "---------------------------Yarn install latest packages---------------------------"
+    );
     const { stdout: stdout2, stderr: stderror2 } = await execPromisified("yarn");
-    // console.log(`stdout: ${stdout2}`);
-    // console.log(`stderr: ${stderror2}`);
+    response.logs.push(stdout2);
+    response.errors.push(stderror2);
 
-    response.msg += stdout2 + "\n";
-    response.error += stderror2 + "\n";
-
+    response.logs.push(
+      "---------------------------Yarn build blitz app---------------------------"
+    );
     const { stdout: stdout3, stderr: stderror3 } = await execPromisified("yarn build");
-    // console.log(`stdout: ${stdout3}`);
-    // console.log(`stderr: ${stderror3}`);
-    response.msg += stdout3 + "\n";
-    response.error += stderror3 + "\n";
+    response.logs.push(stdout3);
+    response.errors.push(stderror3);
 
+    response.logs.push(
+      "---------------------------Apply latest migrations---------------------------"
+    );
     const { stdout: stdout4, stderr: stderror4 } = await execPromisified(
       "npx blitz prisma migrate deploy"
     );
-    // console.log(`stdout: ${stdout4}`);
-    // console.log(`stderr: ${stderror4}`);
+    response.logs.push(stdout4);
+    response.errors.push(stderror4);
 
-    response.msg += stdout4 + "\n";
-    response.error += stderror4 + "\n";
-
+    response.logs.push("---------------------------Restart PM2 process---------------------------");
     const { stdout: stdout5, stderr: stderror5 } = await execPromisified("pm2 restart blitz");
-    // console.log(`stdout: ${stdout5}`);
-    // console.log(`stderr: ${stderror5}`);
-
-    response.msg += stdout5 + "\n";
-    response.error += stderror5 + "\n";
+    response.logs.push(stdout5);
+    response.errors.push(stderror5);
 
     res.status(200).json(response);
   } catch (e) {
     response.statusCode = 500;
-    response.msg = "CI failed";
-    response.error += e.message + "\n";
+    response.logs.push("CI failed");
+    response.errors.push(e.message);
     res.status(response.statusCode).json(response);
   }
 });
