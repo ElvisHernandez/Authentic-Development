1060aba65478e8b8f16c2284a3b3146bb17aa410
Elvis Hernandez <elvishernandezdev@gmail.com>
2023-09-23 22:58:51 -0400

    ci test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,7 +47,7 @@ const HeroSection = () => {
   return (
     <section className="h-screen">
       <div className="font-yellowtail top-[48px] left-[48px] text-4xl absolute text-white hidden xl:block">
-        <Link href="/">AD</Link>
+        <Link href="/">aD</Link>
       </div>
 
       <div className="relative overflow-x-clip hidden xl:block">
